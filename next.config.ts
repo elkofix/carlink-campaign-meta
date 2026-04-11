04c7f74f48d0e807916dcdf63048eb8746769c35
Alejandro Cordoba Erazo <alejitocordoba@hotmail.es>
2026-04-11 16:21:29 -0500

    feat: adding config for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const repoName = 'carlink-campaign-meta';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  trailingSlash: true,
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
